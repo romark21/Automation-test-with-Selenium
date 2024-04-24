b1d29b7c6ab4645cb892e4edaf095c5d7826124b
Romans <romansveds@gmail.com>
2024-04-24 12:50:59 +0300

    In the test, which checks the operation of adding a product to cart, the ability to test ten different product links using pytest.mark.parametrize is implemented

--- config/links.py
+++ config/links.py
@@ -1,5 +1,7 @@
 class Links:
     HOST = "https://selenium1py.pythonanywhere.com"
     MAIN_PAGE = HOST
-    LOGIN_PAGE = f'{HOST}/ru/accounts/login/'
-    PRODUCT_PAGE = f'{HOST}/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    LOGIN_PAGE = f'{HOST}/accounts/login/'
+    PRODUCT_PAGE = f'{HOST}/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    PRODUCT_PAGE_2 = f'{HOST}/catalogue/coders-at-work_207/?promo=newYear2019'
+    PROMO_LINK_TAMPLATE: str = f'{HOST}/catalogue/coders-at-work_207/?promo='
--- tests/test_product_page.py
+++ tests/test_product_page.py
@@ -1,16 +1,27 @@
+import pytest
 from config.links import Links
 from pages.product_page import ProductPage
 from time import sleep
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = Links.PRODUCT_PAGE
+@pytest.mark.parametrize('link',
+                         [f'{Links.PROMO_LINK_TAMPLATE}offer0',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer1',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer2',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer3',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer4',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer5',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer6',
+                          pytest.param(f'{Links.PROMO_LINK_TAMPLATE}offer7',
+                                       marks=pytest.mark.xfail(reason='bug in basket')),
+                          f'{Links.PROMO_LINK_TAMPLATE}offer8',
+                          f'{Links.PROMO_LINK_TAMPLATE}offer9'])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = Links.PRODUCT_PAGE_2
     page = ProductPage(browser, link)
     page.open()
     page.add_to_basket()
     page.solve_quiz_and_get_code()
     page.check_product_name_in_basket()
     page.check_product_price_in_basket()
-
-    sleep(5)
-
+    sleep(2)
